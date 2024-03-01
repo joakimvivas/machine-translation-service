668f01fea22c084b011dbd35af79d936a5bdd92c
Joakim Vivas <kimi@kimtux.net>
2024-03-01 22:22:24 +0100

    Re-invented Application

--- README.md
+++ README.md
@@ -1,85 +1,34 @@
-# Translation flask API for the Helsinki NLP
+# AI Translation flask API for the Helsinki NLP
 
-This is the language translation API based on transformers and MarianMT model for language translation (based on huggingface). This provides easy pretrained Language models that can be used to do machine translation in any 2 desired languages. There is one webpage as well that works on MyMemory API to provide translation that can also store recent translations.  
-
-## API
-There are 2 versions of the  REST API
-version 0 that exposes only supported language and other as webapp
-version 1 that exposes 4 endpoints to the internet.
-
-**V0 API CONTRACTS**
-
-```txt
-GET /
-GET v0/supported_langs
-```
-**V1 API CONTRACTS**
-```txt
-GET v1/health
-GET v1/lang_routes
-GET v1/supported_languages
-POST v1/translate
-```
-**Inputs**
-'source' : Source Language 
-'target' : Target Language
-'text' : Text to be transferred
-
-**OUTPUTS**
-'translation' : Translated Text
-'supported languages' : Languages Supported 
-'language routes' : routes to languages
+This is a **Python Flask application** based on the [Huggingface](https://huggingface.co) transformer library with the "[Helsinki-NLP/opus-mt-en-en-es](https://huggingface.co/Helsinki-NLP/opus-mt-en-es)" model developed by the [Language Technology Research Group of the University of Helsinki](https://blogs.helsinki.fi/language-technology/).
 
 ## Run the Application
 ### To run Locally
 
-0. After this, ensure you have installed virtualenv globally as well. If not, run this:
-```
-$ pip install virtualenv
-```
 1. Cd into your repo as such:
 ```
 $ cd machine-translation-service
 ```
 
-2. Create and fire up your virtual environment in python3:
+2. Install requirements:
 ```
-$ virtualenv -p python3 venv
-$ pip install autoenv
-```
-```
-(venv)$ pip install -r requirements.txt
+$ pip install -r requirements.txt
 ```
 
 3. Run the server using this one simple command:
 ```
-(venv)$ flask run
+$ flask run
 ```
 You can now access the app on your local browser by using
 ```
 http://localhost:5000/
 ```
-Or test using  Postman
 
 ## To run with docker
 
 ```
 docker build -t machine-translation-service .
-docker run -p 5000:5000 -v $(pwd)/data:/app/data -d machine-translation-service
+docker run -p 5000:5000 -d machine-translation-service
 ```
 
-The front end should then become available at http://localhost:5000.
-
-
-## Call the service with curl
-```
-curl --location --request POST 'http://localhost:5000/v1/translate' \
---header 'Content-Type: application/json' \
--d '{
- "text":"hello",
- "source":"en",
- "target":"es"
-}'
-```
-
-curl --location --request GET 'http://localhost:5000/v1/lang_routes'
+The front end should then become available at ```http://localhost:5000```
--- app.py
+++ app.py
@@ -1,80 +1,26 @@
-import os
-from flask import Flask,request,jsonify,render_template
-from model.translator import Translator,HTTPTranslate 
-from config import *
-
-History = dict()
-HistCount=0
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from flask import Flask, request, render_template,jsonify
+tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-en-es")
+model = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-en-es")
 
 app = Flask(__name__)
 
-translator = Translator(MODEL_PATH)
+#inp = "Hi! My name is Joakim, nice to meet you!"
+#input_ids = tokenizer(inp, return_tensors="pt").input_ids
+#outputs = model.generate(input_ids=input_ids, num_beams=1, num_return_sequences=1)
+#print("Generated:", tokenizer.batch_decode(outputs, skip_special_tokens=True))
 
-app.config["DEBUG"] = True # turn off when in production
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-def saveHistory(source,target,text,translation):
-    '''
-    Saves History of the transactions Happened on the WebAPP
-    Input: Source,target,text,translation -> str
-    Output: History -> Dict
-    '''
-    global HistCount
-    newHist=dict()
-    newHist["src"], newHist["tgt"], newHist["txt"], newHist["trans"] = source,target,text,translation
-    History[HistCount] = newHist
-    HistCount += 1
-    return History
+@app.route('/translate', methods=['GET','POST'])
+def my_form_post():
+    input = request.form['input']
+    input_ids = tokenizer(input, return_tensors="pt").input_ids
+    outputs = model.generate(input_ids=input_ids, num_beams=1, num_return_sequences=1)
+    result = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+    return jsonify(result=result)
 
-@app.route('/',methods=["GET","POST"])
-def translateApp():
-    '''
-    Index function to render webapp
-    DEPRECIATION WARNING : Works on v0 API
-    '''
-    if request.method=='GET':
-        return render_template('index.html',supp_langs=SUPPORTED_LANGUAGES,history=History)
-    else:
-        fromLang = request.form.get('sourceLang')
-        toLang = request.form.get('targetLang')
-        text = request.form.get('textToTranslate')
-        translation = HTTPTranslate(text,toLang,fromLang)
-        log = saveHistory(fromLang,toLang,text,translation)
-        return render_template('index.html',transText=translation,supp_langs=SUPPORTED_LANGUAGES,history=log)
-
-@app.route('/v1/health', methods=["GET"])
-def health_check():
-    '''Confirms service is running'''
-    return "Machine translation Service is up and running...."
-
-@app.route('/v1/lang_routes', methods=["GET"])
-def get_lang_route():
-    '''return all the languages routes available'''
-    lang = requests.args['lang']
-    all_lang = translator.get_supported_langs()
-    lang_routes = [la for la in all_langs if la[0]==lang]
-    return jsonify({"language routes":lang_routes})
-
-@app.route('/v1/supported_languages',methods=["GET"])
-def get_supported_languages():
-    '''returns all the supported languages from data'''
-    langs = translator.get_supported_langs()
-    return jsonify({"supported languages":langs})
-
-@app.route('/v0/supported_langs',methods=['GET'])
-def supp_langs():
-    '''v0 API for returning All the supported Languages'''
-    return jsonify({"supported languages":SUPPORTED_LANGUAGES})
-
-@app.route('/v1/translate',methods=["POST"])
-def get_prediction():
-    '''
-    function to return translation based on source and target language
-    '''
-    source = request.json['source']
-    target = request.json['target']
-    text = request.json['text']
-    translation = translator.translate(source,target,text)
-    return jsonify({"translation":translation})
-
-app.run(host="0.0.0.0")
-
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
