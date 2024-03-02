e6c25e8bc636b9b808e6059cb91d4c3061208329
Joakim Vivas <kimi@kimtux.net>
2024-03-02 13:04:48 +0100

    Added Spanish to English

--- app.py
+++ app.py
@@ -2,24 +2,33 @@ from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 from flask import Flask, request, render_template,jsonify
 tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-en-es")
 model = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-en-es")
+tokenizer_es = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-es-en")
+model_es = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-es-en")
 
 app = Flask(__name__)
-
-#inp = "Hi! My name is Joakim, nice to meet you!"
-#input_ids = tokenizer(inp, return_tensors="pt").input_ids
-#outputs = model.generate(input_ids=input_ids, num_beams=1, num_return_sequences=1)
-#print("Generated:", tokenizer.batch_decode(outputs, skip_special_tokens=True))
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/translate', methods=['GET','POST'])
-def my_form_post():
+@app.route('/spanish')
+def spanish():
+    return render_template('spanish.html')
+
+@app.route('/translate_en_es', methods=['GET','POST'])
+def english_form():
     input = request.form['input']
     input_ids = tokenizer(input, return_tensors="pt").input_ids
     outputs = model.generate(input_ids=input_ids, num_beams=1, num_return_sequences=1)
     result = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+    return jsonify(result=result)
+
+@app.route('/translate_es_en', methods=['GET','POST'])
+def spanish_form():
+    input = request.form['input']
+    input_ids = tokenizer_es(input, return_tensors="pt").input_ids
+    outputs = model_es.generate(input_ids=input_ids, num_beams=1, num_return_sequences=1)
+    result = tokenizer_es.batch_decode(outputs, skip_special_tokens=True)
     return jsonify(result=result)
 
 if __name__ == '__main__':
