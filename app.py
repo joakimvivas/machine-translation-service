67f1c825ce52f94208c510ad94cfd079438122a5
Joakim Vivas <kimi@kimtux.net>
2024-03-02 21:37:07 +0100

    Added French to Spanish

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-en-es")
 model = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-en-es")
 tokenizer_es = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-es-en")
 model_es = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-es-en")
+tokenizer_fr = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-fr-es")
+model_fr = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-fr-es")
 
 app = Flask(__name__)
 
@@ -11,9 +13,17 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/spanish')
+@app.route('/english-to-spanish')
+def english():
+    return render_template('index.html')
+
+@app.route('/spanish-to-english')
 def spanish():
     return render_template('spanish.html')
+
+@app.route('/french-to-spanish')
+def french():
+    return render_template('french.html')
 
 @app.route('/translate_en_es', methods=['GET','POST'])
 def english_form():
@@ -29,6 +39,14 @@ def spanish_form():
     input_ids = tokenizer_es(input, return_tensors="pt").input_ids
     outputs = model_es.generate(input_ids=input_ids, num_beams=1, num_return_sequences=1)
     result = tokenizer_es.batch_decode(outputs, skip_special_tokens=True)
+    return jsonify(result=result)
+
+@app.route('/translate_fr_es', methods=['GET','POST'])
+def french_form():
+    input = request.form['input']
+    input_ids = tokenizer_fr(input, return_tensors="pt").input_ids
+    outputs = model_fr.generate(input_ids=input_ids, num_beams=1, num_return_sequences=1)
+    result = tokenizer_fr.batch_decode(outputs, skip_special_tokens=True)
     return jsonify(result=result)
 
 if __name__ == '__main__':
